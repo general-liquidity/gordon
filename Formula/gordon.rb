dd64d0f552564ee4d4165ea1037ecf39893658bb
Tiberiu Toca <tibi.toca@gmail.com>
2026-08-26 14:04:55 +0000

    chore: publish assets for v0.3.1

--- Formula/gordon.rb
+++ Formula/gordon.rb
@@ -9,26 +9,26 @@
 class Gordon < Formula
   desc "The Frontier Trading Agent - AI-powered crypto trading CLI"
   homepage "https://gordoncli.com"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/general-liquidity/gordon/releases/download/v#{version}/gordon-darwin-arm64"
-      sha256 "f65c6f816681ec3980577e9e780b1e02f42b54e07dd3745752f081085c7b72b0"
+      sha256 "0606859249cd91927bdcfed5599bb5e39a76d579b49f465aa91a901a9f90957b"
     else
       url "https://github.com/general-liquidity/gordon/releases/download/v#{version}/gordon-darwin-x64"
-      sha256 "42b17a30c0e696c2b9591f8c5e3e2db65455d8ef5eda6ccd0eb04c0c592de492"
+      sha256 "f8588c99398992fddd62cd918363ee89d523afd9bdf323bd9326b8c2c5185abb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/general-liquidity/gordon/releases/download/v#{version}/gordon-linux-arm64"
-      sha256 "d530d6214e42bb5a16832271b1929e320ec290288e6144d2bb944aa38678827a"
+      sha256 "407343aaf9d79ff3ad6468a882801a5cd4650a04e4cd2dd96c76022b2a3231fd"
     else
       url "https://github.com/general-liquidity/gordon/releases/download/v#{version}/gordon-linux-x64"
-      sha256 "96747e0b784a8aa862ba32b0e772e7416923cada7b777e6b088a8b73a9ab86c5"
+      sha256 "e0b584cca05720d111787dd9f8f426b47ef2f1540ce0e1adaf9d692fc22f9bf8"
     end
   end
 
